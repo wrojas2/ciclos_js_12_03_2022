d5909db354db216b5bbaa20afd503174ec84569f
wrojas2 <wiros2@gmail.com>
2022-03-15 09:00:02 -0500

    FOR IN y FOR OF_15_03_2022

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-for (let i = 0; i <5; i++) {
-    console.log(`Ciclo Normal ${i}`);
-    }
-
-
-let x = 0;
-for (x; true;){
-  if (x<5){
-      console.log(`Ciclo Personalizado: ${x}`);
-      x++;
-  }
-  else{
-      break;
-  }
+let data = new Array(5).fill(null);
+Object.preventExtensions(data);
+data[0] = "Miguel";
+data[1] = "Oscar";
+data[2] = "Juan";
+data[4] = "William";
+console.log(data);
+console.log(``);
+console.log(`Indice`);
+for (let i in data){
+    console.log(`For in: ${i}.`);
 }
-
+console.log(``);
+console.log(`Valor`);
+for (let v of data){
+    console.log(`For of: ${v}`);
+}
+console.log(``);
+console.log(`Indice y Valor`);
+for (let [indice,valor] of Object.entries(data)) {
+    console.log(` Object.entries: ${indice} = ${valor}`);
+}
+    
