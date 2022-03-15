3ad7814ca246ea161a918921fe29b17bad497f4b
wrojas2 <wiros2@gmail.com>
2022-03-14 20:11:54 -0500

    CICLO_FOR_14_03_2022

---
+++ main.js
@@ -0,0 +1,16 @@
+for (let i = 0; i <5; i++) {
+    console.log(`Ciclo Normal ${i}`);
+    }
+
+
+let x = 0;
+for (x; true;){
+  if (x<5){
+      console.log(`Ciclo Personalizado: ${x}`);
+      x++;
+  }
+  else{
+      break;
+  }
+}
+
