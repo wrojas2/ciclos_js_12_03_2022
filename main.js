00d82e6c3587b0d20e20a43c5640ebc5e3b2e5a3
wrojas2 <wiros2@gmail.com>
2022-03-17 17:43:56 -0500

    CICLO FOR EACH 17_03_2022

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 let data = new Array(5).fill(null);
 Object.preventExtensions(data);
-data[0] = "Miguel";
-data[1] = "Oscar";
-data[2] = "Juan";
-data[4] = "William";
-console.log(data);
-console.log(``);
-console.log(`Indice`);
-for (let i in data){
-    console.log(`For in: ${i}.`);
-}
-console.log(``);
-console.log(`Valor`);
-for (let v of data){
-    console.log(`For of: ${v}`);
-}
-console.log(``);
-console.log(`Indice y Valor`);
-for (let [indice,valor] of Object.entries(data)) {
-    console.log(` Object.entries: ${indice} = ${valor}`);
-}
-    
+data.fill("William", 4);
+
+console.log("PRIMER CASO") 
+data.forEach(function(element){  
+    console.log(element) 
+ });
+
+ lista = {
+    nombre: "WILLIAM",
+    apellido: "ROJAS"
+ }
+ console.log(" ") 
+ console.log("SEGUNDO CASO") 
+ // object.Keys permite mostrar los nombres de las variables
+ // object.values permite mostrar el valor de las variables
+ // object.entries
+ // V es el valor, i es el indice y clone clona el array
+ Object.entries(lista).forEach(function(v,i,clone){
+     console.log(clone);      
+ });
+console.log(" ") 
+console.log("TERCER CASO - PROTOTIPO FORECH") 
+addEventListener("submit", function(e){
+   Array.from(e.target).forEach(element=>{
+       if(element.nodeName != "BUTTON"){
+           console.log(element.value);
+       }
+       else{
+           console.log(element.dataset.envio);
+       }       
+    })
+    e.preventDefault();
+})
+
+ 
+ 
