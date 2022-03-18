9e5c31cec51ea9116e615a7f316198d87b3dacac
wrojas2 <wiros2@gmail.com>
2022-03-17 20:34:33 -0500

    CICLO WHILE 17_03_2022

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
-
-// let data = new Array(5).fill(null);
-// Object.preventExtensions(data);
-// data.fill("Miguel", 4);
-function saludar() {
-    return "Hola Como estas";
-}
-let data = {
-    nombre: "William",
-    apellido : "Rojas",
-    edad: 45,
-    saludar
+let papitas = 10;
+console.log(`Ciclo 1`);
+while (papitas){
+  console.log(`Me comí ${papitas} papitas`)
+  papitas--;
 }
 
-let datos = Object.entries(data).map(function(elemt){
-    let obj = {};
-    if(typeof(elemt[1]) == "function"){
-        obj.saludar = data.saludar();
-    }else{
-        if(elemt[0] == "nombre"){
-            obj[elemt[0]] = `${elemt[1].toUpperCase()} Carlos`;
-        }else{
-            obj[elemt[0]] = elemt[1];
-        }
-    }
-    return obj;
-})
-
-console.log(data);
-console.log(Object.assign({}, ...datos) );
-
+console.groupEnd();
+console.group(`Ciclo 2`)
+while (papitas<=10){
+  console.log(`Me comí ${papitas} papitas`)
+  papitas++
+}
+console.groupEnd
