db0f6285c576c29d78a1a9df8b251b98e375f68b
wrojas2 <wiros2@gmail.com>
2022-03-17 21:24:56 -0500

    PROPIEDADES BREAK CONTINUE 17_03_2022

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-var papitas = 1;
-do{
-    console.log(`Me comí ${papitas} papitas`);
-    papitas++;
-}while(papitas<=10)
-console.log(`RESTANDO PAPITAS`);
-papitas = 10
-do{
-    console.log(`Me quedan ${papitas} papitas`);
-     papitas--;
- }while(papitas>0)
+
+numero0Al10:
+for (let i = 0; i<10;i++){  
+    if (i==2 || i== 6){ 
+       console.log("Numero Ingnorado");
+       continue numero0Al10;
+    }
+    console.log(i);
+}
+
+multiplicando:
+for (let i = 1; i<=10;i++){ 
+    console.log(`%c Tabla del ${i}`,"color:red;")
+    multiplicador:
+    for (let x = 1; x<=10; x++){
+        let resultado = i*x;
+        if (resultado%3==0){
+            console.log(`${i} x ${x} = ${i*x} = PAREMOS`);
+            continue multiplicando;
+        } 
+        console.log(`${i} x ${x} = ${resultado}`);
+    }  
+   
+}
